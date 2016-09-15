cdf7a87b496c72c5b157b2609dd43a580d419b49
Daniel Earwicker <dan@earwicker.com>
2016-09-15 13:42:07 +0100

    Much simplified, just bindToStore for now

--- index.ts
+++ index.ts
@@ -1,116 +1,42 @@
 import * as I from "immuto";
 import * as React from "react";
 
-interface SubscriberState<S, A> {
-    inner?: I.Cursor<S, A>;
-}
+/**
+ * Given a store, and a function from cursor to JSX.Element, we return
+ * a ComponentClass. The resulting component has no props - the idea
+ * is to do this at the root, to produce an App component ready to be
+ * rendered.
+ */
 
-class Subscriber<Props, S, A> extends React.Component<Props, SubscriberState<S, A>> {
+export function bindToStore<S, A>(
+    store: I.Store<S, A>,
+    render: (cursor: I.Cursor<S, A>) => JSX.Element
+): React.ComponentClass<{}> {
 
-    private sub?: I.Subscribable;
-    private unsub?: (() => void);
+    return class extends React.Component<{}, { cursor?: I.Cursor<S, A> }> {
 
-    constructor(
-        private renderInner: (props: Props, cursor: I.Cursor<S, A>) => JSX.Element
-    ) {
-        super();
-        this.state = {};
-    }
-
-    subscribe(subscribable: I.Subscribable) {
-        if (subscribable === this.sub) {
-            return;
-        }
-
-        this.unsubscribe();
-        this.sub = subscribable;
-        this.unsub = subscribable.subscribe(() => this.storeChanged());
-    }
-
-    unsubscribe() {
-        if (this.unsub) {
-            this.unsub();
-            this.unsub = undefined;
-            this.sub = undefined;
-        }
-    }
-
-    componentWillUnmount() {
-        this.unsubscribe();
-    }
-
-    storeChanged() {
-        if (this.state.inner) {
-            this.setState({
-                inner: this.state.inner.refresh()
-            });
-        }
-    }
-
-    render() {
-        return this.state.inner
-            ? this.renderInner(this.props, this.state.inner)
-            : null;
-    }
-}
-
-export function bindToStore<P, S, A>(
-    render: (props: P, cursor: I.Cursor<S, A>) => JSX.Element,
-    store: I.Store<S, A>
-): React.ComponentClass<P> {
-
-    return class extends Subscriber<P, S, A> {
+        private unsub?: (() => void);
 
         constructor() {
-            super(render);
+            super();
+
+            this.unsub = store.subscribe(() => this.storeChanged());
+            this.state = { cursor: I.snapshot(store) };
         }
 
-        componentWillMount() {
-            this.setState({
-                inner: I.snapshot(store)
-            });
-
-            this.subscribe(store);
-        }
-    }
-}
-
-export interface CursorProps<S, A, K> {
-    cursor: I.Cursor<S, A>;
-    path: K;
-}
-
-export function bindToCursor<IS, IA, OS, OA, P, K>(
-    render: (props: P, cursor: I.Cursor<IS, IA>) => JSX.Element,
-    traverse: (outer: I.Cursor<OS, OA>, path: K) => I.Cursor<IS, IA>
-): React.ComponentClass<P & CursorProps<OS, OA, K>> {
-
-    type Props = P & CursorProps<OS, OA, K>;
-
-    return class extends Subscriber<Props, IS, IA> {
-
-        constructor() {
-            super(render);
-        }
-
-        updateFromProps(newProps: Props) {
-            this.subscribe(newProps.cursor.subscribable);
-
-            const traversed = traverse(newProps.cursor, newProps.path);
-
-            if (!this.state.inner || traversed.state !== this.state.inner.state) {
-                this.setState({
-                    inner: traversed
-                });
+        componentWillUnmount() {
+            if (this.unsub) {
+                this.unsub();
+                this.unsub = undefined;
             }
         }
 
-        componentWillMount() {
-            this.updateFromProps(this.props);
+        storeChanged() {
+            this.setState({ cursor: I.snapshot(store) });
         }
 
-        componentWillUpdate(nextProps: Props) {
-            this.updateFromProps(this.props);
+        render() {
+            return this.state.cursor ? render(this.state.cursor) : null;
         }
     }
 }
